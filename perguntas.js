542be2e8b98ed11a8a62a781bfd97bab8ccaa4ac
LarissaManoelaCavaquinho <163007842+LarissaManoelaCavaquinho@users.noreply.github.com>
2024-10-21 07:32:40 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Brasil' ,
+    'Quem descobriu o Brasil?' ,
+    'Jonh Vlogs'
+)
+
+criaCartao(
+    'Fortuna' ,
+    'Quem é a pessoa mais rica do mundo?' ,
+    'Elon Musk'
+)
+
+criaCartao(
+    'Rei Leão' ,
+    'Qual o animal principal do Rei Leão?' ,
+    'Cabra'
+)
+
+criaCartao(
+    'Descoberta' ,
+    'Quem descobriu a America' ,
+    'P. Diddy'
+)
+
+criaCartao(
+    'Grota' ,
+    'Quem é dono da Grota no Rp?' ,
+    'Douglinhas'
+)
+
+criaCartao(
+    'Distante' ,
+    'Qual pais mais distante do Brasil' ,
+    'Peru'
+)
